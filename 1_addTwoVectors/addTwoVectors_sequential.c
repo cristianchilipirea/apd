792372b2b0e9ebe60765aaa3c37057fa3aef4ca8
cristian.chilipirea <cristian.chilipirea@gmail.com>
2018-08-09 19:31:17 +0300

    added makefile

--- 1_addTwoVectors/addTwoVectors_sequential.c
+++ 1_addTwoVectors/addTwoVectors_sequential.c
@@ -66,21 +66,9 @@ void print()
 int main(int argc, char **argv)
 {
 	int i;
-	pthread_t tid[P];
 
 	init();
 
-	int thread_id[P];
-	for(i = 0;i < P; i++)
-		thread_id[i] = i;
-
-	for(i = 0; i < P; i++) {
-		pthread_create(&(tid[i]), NULL, threadFunction, &(thread_id[i]));
-	}
-
-	for(i = 0; i < P; i++) {
-		pthread_join(tid[i], NULL);
-	}
 
 	print();
 
